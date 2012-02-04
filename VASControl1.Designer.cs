5a9c6d73991a8e5f16dbe6b924c785749ac65fa0
Tom Wright <tom@tdwright.co.uk>
2012-02-04 19:04:39 +0000

    Initial commit. Works OK.

---
+++ VASControl1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace VAS
+{
+    partial class VAS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar
+            // 
+            this.trackBar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trackBar.LargeChange = 1;
+            this.trackBar.Location = new System.Drawing.Point(0, 0);
+            this.trackBar.Maximum = 1000;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(483, 30);
+            this.trackBar.TabIndex = 0;
+            this.trackBar.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.trackBar_MouseDown);
+            // 
+            // VAS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.trackBar);
+            this.Name = "VAS";
+            this.Size = new System.Drawing.Size(483, 30);
+            this.Load += new System.EventHandler(this.VAS_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar;
+    }
+}
---
+++ VASControl1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VAS
+{
+    public partial class VAS : UserControl
+    {
+        public VAS()
+        {
+            InitializeComponent();
+        }
+
+        private int max = 100;
+
+        public int Maximum
+        {
+            get
+            {
+                return max;
+            }
+            set
+            {
+                max = value;
+                trackBar.Maximum = value;
+            }
+        }
+
+        private int min = 0;
+
+        public int Minimum
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                min = value;
+                trackBar.Minimum = value;
+            }
+        }
+
+        public int Value
+        {
+            get
+            {
+                return this.trackBar.Value;
+            }
+        }
+
+        public new bool Enabled
+        {
+            get
+            {
+                return trackBar.Enabled;
+            }
+            set
+            {
+                trackBar.Enabled = value;
+            }
+        }
+
+        private void trackBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            int marginsize = 14;
+            double leftDelta = e.Location.X - (this.trackBar.Location.X + marginsize);
+            int newvalue = (int)(leftDelta / (this.trackBar.Size.Width - (marginsize * 2)) * this.trackBar.Maximum);
+            this.trackBar.Value = clip(newvalue,this.trackBar.Minimum,this.trackBar.Maximum);
+        }
+
+        private int clip(int value, int min, int max)
+        {
+            if (value < min) return min;
+            if (value > max) return max;
+            return value;
+        }
+
+        private void VAS_Load(object sender, EventArgs e)
+        {
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            this.trackBar.Minimum = this.Minimum;
+            this.trackBar.Maximum = this.Maximum;
+            this.trackBar.Value = (int)(0.5 * (this.Minimum + this.Maximum));
+        }
+
+        public event MouseEventHandler Accessed
+        {
+            add { this.trackBar.MouseDown += value; }
+            remove { this.trackBar.MouseDown -= value; }
+        }
+
+
+    }
+}
